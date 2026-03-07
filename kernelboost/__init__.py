076df760cbc4a9b48d8cef42a1f1e8d35df80a7b
tuomas <tuomas.s.laiho@gmail.com>
2026-03-07 10:22:05 +0200

    version numbers updated to all files.

--- kernelboost/__init__.py
+++ kernelboost/__init__.py
@@ -1,6 +1,6 @@
 """KernelBooster: Gradient boosting with Nadaraya-Watson (local constant) estimator as base learners."""
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from .booster import KernelBooster
 from .multiclassbooster import MulticlassBooster
